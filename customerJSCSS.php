e6b76fb12d5e52a5f170226f3f0bb2a08913bc0c
LiJohnson <598420668@163.com>
2013-07-11 11:28:22 +0000

    优化url的前后的加载范围
    git-svn-id: https://svn.sinacloud.com/theotherdoor/1/wp-content/plugins/customerJSCSS@533 8ae2ba1d-993d-4c9a-aded-4246d96bdaa4

--- customerJSCSS.php
+++ customerJSCSS.php
@@ -20,87 +20,97 @@ class CustomCssAndScript
 	{
 		$this->fileType = array('css' => 'css' , 'js'=>'javascript');
 		$data = $this->getOption();
-		if( $data['setting']['backEnd'] )add_action('admin_head', array($this,"headAction"));
-		if( $data['setting']['frontEnd'] )add_action('wp_head', array($this,"headAction"));
+		add_action('admin_head', array($this,"headAction"));
+		add_action('wp_head', array($this,"headAction"));
 		add_action('admin_menu', array($this,"adminMenuAction"));
 	}
 	
 	function headAction($a)
 	{
 		$data = $this->getOption();
-		
-		foreach( $data as $file )
-		{
-			if( $file['type'] === 'css' )
-			{
+		$s = $this->isFrontPage() ? 'frontEnd' : 'backEnd';
+		foreach( $data as $file ){
+			if( $s != $file['frontEnd'] && $s != $file['backEnd'] ){
+				continue;
+			}
+			if( $file['type'] === 'css' ){
 				echo '<link rel="stylesheet" href="'.$file['url'].'">';
 			}
-			else if( $file['type'] === 'js' )
-			{
+			else if( $file['type'] === 'js' ){
 				echo '<script src="'.$file['url'].'" ></script>';
 			}
 		}
 	}
 	
-	function adminMenuAction()
-	{
+	function adminMenuAction(){
 		add_options_page('CSS&JS','CSS&JS',1,1,array($this, 'optionPage'));
 	}
 	
-	function optionPage()
-	{
+	function optionPage(){
 		$data = $this->getOption();
-		
+		$edit = array();
+
 		$url = trim($_POST['url']);
-		if(isset($_POST['add']) && strlen( $url ) > 0)
-		{
-			$data[$url] = array('type'=>$_POST['type'] , 'url'=>$url);
+		if(isset($_POST['add']) && strlen( $url ) > 0){			
+			$data[md5($url)] = array('type'=>$_POST['type'] , 'url'=>$url ,'frontEnd'=>$_POST['frontEnd'],'backEnd'=>$_POST['backEnd']);
 		}
-		if( isset($_POST['delete'] ))
-		{
+
+		if( isset($_POST['delete'] )){
 			unset($data[$_POST['delete']]);
 		}
-		if( isset($_POST['setting']) )
-		{
-			$data['setting']['frontEnd'] = !!$_POST['frontEnd'];
-			$data['setting']['backEnd'] = !!$_POST['backEnd'];
+		if( isset($_POST['edit']) ){
+			$edit = $data[$_POST['edit']];
 		}
-		update_option('CustomCssAndScript' , $data);
-		//var_dump($data);
+
+		$this->updateOption($data);
 		?>
-		<form method=post>
-			前台<input type=checkbox name=frontEnd <?php echo $data['setting']['frontEnd']?'checked':'';?> />
-			后台<input type=checkbox name=backEnd <?php echo $data['setting']['backEnd']?'checked':'';?>  />
-			<input type=submit name=setting class=button-primary value=设置 />
-		</form>
-		<hr>
+		<style>
+			#customJSCSS td,#customJSCSS th{border-left-color:#dfdfdf;border-left-width: 1px;}
+			#customJSCSS table{width:80%}
+			#customJSCSS table form {display: inline;}
+			#customJSCSS input[type=url]{width: 50%;}
+		</style>
+		<div class="dbx-box suf-widget widget_archive"  id='customJSCSS' >
+		<br>
 		<form method=post >
 			<select name=type>
 				<?php foreach($this->fileType as $k => $v){echo "<option value=$k>$v</option>";} ?>
 			</select>
-			<input type=url name=url placeholder=url... />
-			<input type=hidden name=add value=1 />
+			<input type=url name=url placeholder=url... value="<?php echo $edit['url']; ?>" />
+			<label><input type=checkbox name=frontEnd value=frontEnd <?php echo $edit['frontEnd'] ? 'checked':''; ?> />前台</label>
+			<label><input type=checkbox name=backEnd value=backEnd <?php echo $edit['backEnd'] ? 'checked':''; ?> />后台</label>
+			<input type=hidden name=add value=add />
 			<input type=submit class='button-primary' value=添加 />
 		</form>
+		<hr>
 		<?php
-		$td_style = 'style="border-width:1px;border-bottom-style:solid;"';
-		echo '<table style="width:50%">';
-		foreach( $data as $key => $file )
-		{
-			if($key=='setting')continue;
+		$td_style =''; 'style="border-width:1px;border-bottom-style:solid;"';
+		echo '<table class="wp-list-table widefat plugins" >';
+		echo "<tr><th>类型</th><th>url</th><th>作用域</th><th>操作</th></tr>";
+		foreach( $data as $key => $file ){
 			echo '<tr>';
-			echo "<td $td_style ><b>$file[type]</b></td><td $td_style title='$file[url]'>$file[url]</td><td $td_style width=30 ><form method=post ><input type=hidden name=delete value=$key /><input type=submit value=删除 /></form></td>";;
+			echo "<td ><b>$file[type]</b></td> <td ><a href='$file[url]' >$file[url]</a> </td><td>$file[frontEnd] $file[backEnd]</td> <td >";
+			echo "<form method=post ><input type=hidden name=edit value=$key /><input type=submit value=编辑 /></form>";
+			echo "<form method=post ><input type=hidden name=delete value=$key /><input type=submit value=删除 /></form>";
+			echo "</td>";
 			echo '</tr>';
 		}
-		echo '</table>';
+		echo '</table></div>';
 	}
 	
-	private function getOption()
-	{
+	private function getOption(){
 		$data = get_option('CustomCssAndScript');
 		if( !$data )$data = array();
-		if( !$data['setting'] )$data['setting'] = array();
+		//if( !$data['setting'] )$data['setting'] = array();
 		return $data;
+	}
+	private function updateOption($data){
+		update_option('CustomCssAndScript' , $data);
+		return $data;
+	}
+
+	private function isFrontPage(){
+		return is_front_page() || is_page() ||is_single()||is_archive()||is_search();
 	}
 }
 }
